1ad3a5c6d5acc1109c4c085eb16bb833f56b1968
Subilan <suyuanliang1995@gmail.com>
2024-01-18 20:11:51 +0800

    refactor

--- forge/src/main/java/cc/seati/NameWhitelist/forge/Main.java
+++ forge/src/main/java/cc/seati/NameWhitelist/forge/Main.java
@@ -1,15 +1,14 @@
 package cc.seati.NameWhitelist.forge;
 
 import me.shedaniel.architectury.platform.forge.EventBuses;
-import cc.seati.NameWhitelist.Main;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(Main.MOD_ID)
-public class ExampleModForge {
-    public ExampleModForge() {
+@Mod(cc.seati.NameWhitelist.Main.MOD_ID)
+public class Main {
+    public Main() {
         // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(Main.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-        Main.init();
+        EventBuses.registerModEventBus(cc.seati.NameWhitelist.Main.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        cc.seati.NameWhitelist.Main.init();
     }
 }
