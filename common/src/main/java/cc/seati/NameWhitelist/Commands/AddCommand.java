448d0f2e1e571b39fd74ef44154d5bae64243c9b
Subilan <suyuanliang1995@gmail.com>
2024-01-19 13:40:15 +0800

    feat: bulk adding & removing

--- common/src/main/java/cc/seati/NameWhitelist/Commands/AddCommand.java
+++ common/src/main/java/cc/seati/NameWhitelist/Commands/AddCommand.java
@@ -5,14 +5,23 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.TextComponent;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AddCommand {
-    public static int exec(CommandSourceStack source, String name) throws CommandSyntaxException {
-        if (WhitelistFile.containsName(name)) {
-            source.sendFailure(new TextComponent(name + " is already in name whitelist"));
+    public static int exec(CommandSourceStack source, String names) throws CommandSyntaxException {
+        String[] nameList = names.split(" ");
+        List<String> notInList = Arrays.stream(nameList).filter(s -> !WhitelistFile.containsName(s)).collect(Collectors.toList());
+        if (notInList.isEmpty()) {
+            source.sendFailure(new TextComponent("All of the names are already in the name whitelist."));
             return 0;
         }
-        WhitelistFile.addPlayer(name);
-        source.sendSuccess(new TextComponent("Added " + name + " to name whitelist."), false);
+        notInList.forEach(WhitelistFile::addPlayer);
+        int ignoredCount = nameList.length - notInList.size();
+        if (ignoredCount > 0)  source.sendSuccess(new TextComponent("Added " + notInList.size() + " names to name whitelist, " + (nameList.length - notInList.size()) + " ignored."), false);
+        else if (notInList.size() == 1) source.sendSuccess(new TextComponent("Added " + notInList.get(0) + " to name whitelist."), false);
+        else source.sendSuccess(new TextComponent("Added " + notInList.size() + " names to name whitelist."), false);
         return 1;
     }
 }
--- common/src/main/java/cc/seati/NameWhitelist/Commands/RemoveCommand.java
+++ common/src/main/java/cc/seati/NameWhitelist/Commands/RemoveCommand.java
@@ -5,14 +5,23 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.TextComponent;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RemoveCommand {
-    public static int exec(CommandSourceStack source, String name) throws CommandSyntaxException {
-        if (!WhitelistFile.containsName(name)) {
-            source.sendFailure(new TextComponent(name + " is not in name whitelist"));
+    public static int exec(CommandSourceStack source, String names) throws CommandSyntaxException {
+        String[] nameList = names.split(" ");
+        List<String> inList = Arrays.stream(nameList).filter(WhitelistFile::containsName).collect(Collectors.toList());
+        if (inList.isEmpty()) {
+            source.sendFailure(new TextComponent("All of the names are not the name whitelist."));
             return 0;
         }
-        WhitelistFile.removePlayer(name);
-        source.sendSuccess(new TextComponent("Removed " + name + " from name whitelist."), false);
+        inList.forEach(WhitelistFile::removePlayer);
+        int ignoredCount = nameList.length - inList.size();
+        if (ignoredCount > 0)  source.sendSuccess(new TextComponent("Removed " + inList.size() + " names from name whitelist, " + (nameList.length - inList.size()) + " ignored."), false);
+        else if (inList.size() == 1) source.sendSuccess(new TextComponent("Removed " + inList.get(0) + " from name whitelist."), false);
+        else source.sendSuccess(new TextComponent("Removed " + inList.size() + " names from name whitelist."), false);
         return 1;
     }
 }
