5fe0daa5195a5da7c796c6198c0b57a3e3c78559
Subilan <suyuanliang1995@gmail.com>
2024-01-19 14:16:33 +0800

    chore: fabric

--- fabric/src/main/java/cc/seati/NameWhitelist/fabric/Main.java
+++ fabric/src/main/java/cc/seati/NameWhitelist/fabric/Main.java
@@ -1,11 +1,10 @@
 package cc.seati.NameWhitelist.fabric;
 
-import cc.seati.NameWhitelist.Main;
 import net.fabricmc.api.ModInitializer;
 
-public class NameWhitelistFabric implements ModInitializer {
+public class Main implements ModInitializer {
     @Override
     public void onInitialize() {
-        Main.init();
+        cc.seati.NameWhitelist.Main.init();
     }
 }
--- forge/src/main/java/cc/seati/NameWhitelist/forge/Main.java
+++ forge/src/main/java/cc/seati/NameWhitelist/forge/Main.java
@@ -7,7 +7,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(cc.seati.NameWhitelist.Main.MOD_ID)
 public class Main {
     public Main() {
-        // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(cc.seati.NameWhitelist.Main.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         cc.seati.NameWhitelist.Main.init();
     }
